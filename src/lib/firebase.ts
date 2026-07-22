9ae47b103639e224567c43dd32020753ae72022a
TechZone <officialtechzonegh@gmail.com>
2026-07-22 16:23:01 +0000

    Safely initialize Firebase with fallbacks

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCmdC5h4a06FZOvGe9gsXAi18aKrpw4Wt8",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "web-techzone.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "web-techzone",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "web-techzone.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "395849305846",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:395849305846:web:442e16caf68b7cc82666d7",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-C45BTPLJ30"
 };
 
 // Initialize Firebase
